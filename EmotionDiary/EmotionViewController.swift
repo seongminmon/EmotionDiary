2c81e62d0b190a4a51fce518777011ec79cb3c7b
seongmin kim <k2417000@naver.com>
2024-05-20 16:09:59 +0900

    에셋 추가 & 감정일기

--- EmotionDiary/EmotionViewController.swift
+++ EmotionDiary/EmotionViewController.swift
@@ -8,22 +8,52 @@
 import UIKit
 
 class EmotionViewController: UIViewController {
-
+    
+    @IBOutlet var buttonList: [UIButton]!
+    @IBOutlet var labelList: [UILabel]!
+    
+    let wordList: [String] = [
+        "행복해",
+        "사랑해",
+        "좋아해",
+        "당황해",
+        "속상해",
+        "우울해",
+        "심심해",
+        "행복해",
+        "행복해"
+    ]
+    
+    var countList = [Int](repeating: 0, count: 9)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "감정 다이어리"
+        navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        for i in 0..<buttonList.count {
+            setupButton(index: i)
+            setupLabel(index: i)
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupButton(index: Int) {
+        let image = UIImage(named: "slime\(index+1)")?.withRenderingMode(.alwaysOriginal)
+        buttonList[index].setImage(image, for: .normal)
+        
+        // 버튼 태그 설정
+        buttonList[index].tag = index
     }
-    */
-
+    
+    func setupLabel(index: Int) {
+        labelList[index].text = "\(wordList[index]) \(countList[index])"
+        labelList[index].textAlignment = .center
+    }
+    
+    @IBAction func emotionButtonTapped(_ sender: UIButton) {
+        countList[sender.tag] += 1
+        labelList[sender.tag].text = "\(wordList[sender.tag]) \(countList[sender.tag])"
+    }
 }
